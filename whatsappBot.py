f0c5efa3d9babb8e9d5b31b6ee9626c578b78997
Jeferson Morais <mx.jeferson.12@gmail.com>
2019-08-21 12:16:49 -0300

    1.1: whatsapp auto-message fixes

--- whatsappBot.py
+++ whatsappBot.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 
 while True:
-    driver = webdriver.Chrome("../selenium/webdriver/chromedriver.exe")
+    driver = webdriver.Chrome("webdriver/chromedriver.exe")
     driver.get('http://web.whatsapp.com')
 
     nome = input('Entre com o nome do usuario ou grupo: ')
@@ -14,14 +14,15 @@ while True:
     user = driver.find_element_by_xpath(f'//span[@title = "{nome}"]')
     user.click()
 
-    msg_box = driver.find_element_by_class_name('_1Plpp')
+    msg_box = driver.find_element_by_class_name('_3u328')
 
     for i in range(quantidade):
         msg_box.send_keys(msg)
-        button = driver.find_element_by_class_name('_35EW6')
+        button = driver.find_element_by_class_name('_3M-N-')
         button.click()
 
     continua = str(input("Você deseja continuar? [S/N]")).upper().strip()[0]
 
     if continua == "N":
         exit()
+
